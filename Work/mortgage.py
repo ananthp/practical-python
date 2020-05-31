edc8901a08dbe8b3ec20a5e1a13c2e9f27264c1d
Ananth P <ananth.pattabiraman@gmail.com>
2020-05-31 11:06:25 +0530

    ex 1.9 version of mortgage

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,24 +1,30 @@
 # mortgage.py
 #
-# Exercise 1.8
+# Exercise 1.9
 
 principal = 500_000.0
 rate = 0.05
 regular_payment = 2684.11
-extra = 1000
+
+# months are counted from 1, i.e., the first payment happens in month 1
+extra_payment_start_month = 60  # inclusive
+extra_payment_end_month = 108   # exclusive
+extra_payment = 1000
 total_paid = 0.0
 
+# this still has to count from zero
 months = 0
-
 while principal > 0:
-    if months < 12:
-        payment = regular_payment + extra
+    # We increment months at the beginning so the logic looks natural.
+    # Think that the month has arrived, then we proceed with payment. 
+    months = months + 1
+    if months >= extra_payment_start_month and months < extra_payment_end_month:
+        payment = regular_payment + extra_payment
     else:
         payment = regular_payment
 
     principal = principal * (1+rate/12) - payment
     total_paid = total_paid + payment
-    months = months + 1
 
 print('Total paid', total_paid)
 print('Months', months)
