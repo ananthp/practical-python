b2288d634754e7095e1336b77bd0f59af8aa834f
Ananth P <ananth.pattabiraman@gmail.com>
2020-05-31 11:17:08 +0530

    ex 1.10 version of mortgage
    * also made the `extra_payment_end_month` inclusive. In the previous
    exercise this statement was given:
    
    > How much will Dave pay if he pays an extra $1000/month for 4 years starting in year 5 of the mortgage?
    
    along with a code snippet
    
    ```
    extra_payment_start_month = 60
    extra_payment_end_month = 108
    ```
    
    From these it's clear the start month is inclusive and end month is
    exclusive. However, made the end month inclusive to match the result
    shown in the course in this exercise.

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,6 +1,6 @@
 # mortgage.py
 #
-# Exercise 1.9
+# Exercise 1.10
 
 principal = 500_000.0
 rate = 0.05
@@ -8,7 +8,7 @@ regular_payment = 2684.11
 
 # months are counted from 1, i.e., the first payment happens in month 1
 extra_payment_start_month = 60  # inclusive
-extra_payment_end_month = 108   # exclusive
+extra_payment_end_month = 108   # also inclusive. but why?
 extra_payment = 1000
 total_paid = 0.0
 
@@ -18,13 +18,15 @@ while principal > 0:
     # We increment months at the beginning so the logic looks natural.
     # Think that the month has arrived, then we proceed with payment. 
     months = months + 1
-    if months >= extra_payment_start_month and months < extra_payment_end_month:
+    if months >= extra_payment_start_month and months <= extra_payment_end_month:
         payment = regular_payment + extra_payment
     else:
         payment = regular_payment
 
-    principal = principal * (1+rate/12) - payment
+    principal = principal * (1+rate/12)- payment
     total_paid = total_paid + payment
+
+    print(months, round(total_paid, 2), round(principal,2))
 
 print('Total paid', total_paid)
 print('Months', months)
