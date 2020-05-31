0a9a40ffd1067b551a4e5188da41cf8e2c467f83
Ananth P <ananth.pattabiraman@gmail.com>
2020-05-31 10:41:40 +0530

    ex 1.8 version of morgage

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,14 +1,24 @@
 # mortgage.py
 #
-# Exercise 1.7
+# Exercise 1.8
 
 principal = 500_000.0
 rate = 0.05
-payment = 2684.11
+regular_payment = 2684.11
+extra = 1000
 total_paid = 0.0
 
+months = 0
+
 while principal > 0:
+    if months < 12:
+        payment = regular_payment + extra
+    else:
+        payment = regular_payment
+
     principal = principal * (1+rate/12) - payment
     total_paid = total_paid + payment
+    months = months + 1
 
 print('Total paid', total_paid)
+print('Months', months)
