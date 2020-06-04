b48e675fd31a94c581a427402551c6b092ff9047
Ananth P <ananth.pattabiraman@gmail.com>
2020-06-04 20:28:10 +0530

    ex 1.27 processing csv

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,13 @@
 # pcost.py
 #
 # Exercise 1.27
+
+f = open('Data/portfolio.csv', 'rt')
+headers = next(f)
+
+total_cost = 0
+for line in f:
+    row = line.strip().split(',')
+    total_cost = total_cost + int(row[1]) * float(row[2])
+
+print('Cost of the portfolio is ', total_cost)
